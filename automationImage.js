01aca7dfeb2362b26438e11e79c5495203f119bb
Taran <taranpal.singh23@gmail.com>
2019-08-22 20:21:10 +0530

    changes made to the config file's struture

--- automationImage.js
+++ automationImage.js
@@ -1,26 +1,29 @@
-// create a config file (config.js) in the same folder and copy the below coommented lines.
 
-// module.exports ={
+const jsonBody = require('./config')
+var request = require('request');
+
+// create a config file (config.js) in the same folder and copy the below commented lines.
+//
+// let bufferJson ={
 //     "access_token" : "..",    // your access_token for buffer api's app.
-//     "text": "..",     // text that you want to post to your accounts
+//     "text": "This is an auto generated image!!! #automation",     // text that you want to post to your accounts
 //     "profile_ids": ["..", ".."],   // profile ids of you social media accounts
 // }
-
+// 
 // let myJson = {
 //   "jobtitle":"Full Stack developer",
 //   "companyName":"LoopCV",
 //   "location":"Greece"    
 // }
 // exports.myJson = myJson;
-
+// exports.bufferJson = bufferJson;
+//
 // and replace all the "..", with your own credentials/requirements. 
 
-const jsonBody = require('./config')
-var request = require('request');
 
 // styling for the image
-let width = 500;
-let height = 500;
+let width = 400;
+let height = 400;
 let color = "FFF";
 
 let imageNameStart = "https://res.cloudinary.com/dclncnhmm/image/upload/"
@@ -33,7 +36,7 @@ url += "l_text:Arial_20:";
 for(let key in jsonBody.myJson){
   url += key;
   url += " : ";
-  url += myJson[key]; 
+  url += jsonBody.myJson[key]; 
   url += "%0A%0A";
 }
 
@@ -41,20 +44,21 @@ url = url + ",co_rgb:" + color;
 
 finalurl = imageNameStart + url + imageNameEnd;  // cloudinary url for our image
 
+console.log("Finalurl : "+finalurl);
 ////////////////////////////////////////////////////////////////////////////////////////
 
-let text = "text="+jsonBody.text+"&now=true&media="+finalurl;
+let text = "text="+jsonBody.bufferJson.text+"&now=true&media[link]="+finalurl;
 
-jsonBody.profile_ids.forEach(element => {
+jsonBody.bufferJson.profile_ids.forEach(element => {
   text =  text + "&" + "profile_ids[]=" + element; 
 });
 
 var options = {
   method: 'post',
   body: text, 
-  url: 'https://api.bufferapp.com/1/updates/create.json?access_token='+jsonBody.access_token,
+  url: 'https://api.bufferapp.com/1/updates/create.json?access_token='+jsonBody.bufferJson.access_token,
   headers: {
-    "Content-Type":"application/x-www-form-urlencoded"
+    "Content-Type":"application/x-www-form-urlencoded",
   }
 }
 
