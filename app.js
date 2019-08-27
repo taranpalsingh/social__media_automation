051ea302b8667135b720f006a708a26bc08ed2b1
Taran <taranpal.singh23@gmail.com>
2019-08-27 15:28:11 +0530

    Image aligned, scheduling enabled.

--- app.js
+++ app.js
@@ -8,12 +8,14 @@
 // }
 // 
 //   const myJson = {
-//     "backgroundImage":"https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+//     "backgroundImage":"download.jpg",
 //     "jobtitle":"Backend Developer",
 //     "companyName":"LoopCV",
-//     "location":"Greece"
-//   }
-  
+//     "location":"Greece",
+//     "scheduled_at": ""  // For posting it right now, just leave it like this
+//                         // For scheduling, give the timestamp in utc. 
+//                          
+//   }  
 //   const cloudinaryJson = { 
 //     cloud_name: '..', 
 //     api_key: '..', 
@@ -22,9 +24,6 @@
 //   exports.myJson = myJson;
 //   exports.bufferJson = bufferJson;
 //   exports.cloudinaryJson = cloudinaryJson;
-  
-
-
 
 const jsonBody = require('./config'); 
 const puppeteer = require('puppeteer');
@@ -36,6 +35,7 @@ const request = require('request');
 
 const app = express();
 
+console.log(jsonBody.myJson.companyName);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
@@ -51,35 +51,43 @@ ejs.renderFile(__dirname + "/index.ejs", jsonBody.myJson, (err, data)=> {
         (async () => {
             const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
             const page = await browser.newPage();
-            await page.goto('file:///C:/Users/taran.singh.CYBERGINDIA/Desktop/projects/greeceAutomation/index.html');
+            await page.goto(__dirname + "/index.html");
+            await page.setViewport({
+                width: 600,
+                height: 400,
+                deviceScaleFactor: 1,
+            })
             await page.screenshot({path: 'screenshot.png'});
             console.log("screenshot saved");  
-            cloudinary.v2.uploader.upload("screenshot.png", (error, result)=> { 
-                if(error)   console.log(error)
-                console.log("Image URL: " + result.url)
-                console.log("Image URL created");
-
-                let text = "text="+jsonBody.bufferJson.text+"&now=true&media[photo]=" + imageUrl;
+            // cloudinary.v2.uploader.upload("screenshot.png", (error, result)=> { 
+            //     if(error)   console.log(error)
+            //     imageUrl = result.url;
+            //     console.log("Image URL: " + imageUrl);
+            //     console.log("Image URL created");
+            //     // &scheduled_at=1566889800
+            //     let text = "text="+jsonBody.bufferJson.text+"&now=true&media[photo]=" + imageUrl;
+            //     if(jsonBody.myJson.scheduled_at !== "")
+            //         text += "&scheduled_at=" + jsonBody.myJson.scheduled_at;
                 
-                jsonBody.bufferJson.profile_ids.forEach(element => {
-                text =  text + "&" + "profile_ids[]=" + element; 
-                });
+            //     jsonBody.bufferJson.profile_ids.forEach(element => {
+            //     text =  text + "&" + "profile_ids[]=" + element; 
+            //     });
                 
-                let options = {
-                    method: 'post',
-                    body: text, 
-                    url: 'https://api.bufferapp.com/1/updates/create.json?access_token='+jsonBody.bufferJson.access_token,
-                    headers: {"Content-Type":"application/x-www-form-urlencoded"},
-                }
-                // POST call to buffer api
-                request(options, (err, res, body)=> {
-                if (err) {
-                    console.log('Error :', err);
-                    return
-                }
-                console.log(' Body :', body);
-                });
-            });            
+            //     let options = {
+            //         method: 'post',
+            //         body: text, 
+            //         url: 'https://api.bufferapp.com/1/updates/create.json?access_token='+jsonBody.bufferJson.access_token,
+            //         headers: {"Content-Type":"application/x-www-form-urlencoded"},
+            //     }
+            //     // POST call to buffer api
+            //     request(options, (err, res, body)=> {
+            //     if (err) {
+            //         console.log('Error :', err);
+            //         return
+            //     }
+            //     console.log(' Body :', body);
+            //     });
+            // });            
             await browser.close();
         })();
     })
