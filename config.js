827da4f5dad5dddc5e3a2500d4778bf9b4fc04d0
Taran <taranpal.singh23@gmail.com>
2019-09-01 11:12:09 +0530

    uploading config file

---
+++ config.js
@@ -0,0 +1,27 @@
+const bufferJson ={
+  "access_token" : "2/4e63a0d53b91746132e844e5567c80feb383e85ae643c30d404c8016997c286b8c0e8a4bcc6778afff47bd7f567f71c2b3598e84dd44b1ab59e8a1106937a549",
+  "accounts" : {
+    "instagram" : "5d582feef5f92a3e3d30adf7",
+    "facebook" : "5d5c01b234f95b5f8d07ba36",
+    "twitter" : "5d58300df5f92a5a701384e3"
+}}
+
+const myJson = {  
+  "font_family":"fantasy",
+  "text": "right now with image aligned!!! #automation #bufferApi",
+  "backgroundImage":"download.jpg",
+  "jobtitle":"Finance",
+  "companyName":"Cybergroup",
+  "location":"Greece",
+  "scheduled_at": "",
+  "postsTo" : ["twitter"]
+}
+
+const cloudinaryJson = { 
+  cloud_name: 'dclncnhmm', 
+  api_key: '792376593158319', 
+  api_secret: 'sKTr-LUijm5wGGzh1hHri9_7l-E' 
+}
+exports.myJson = myJson;
+exports.bufferJson = bufferJson;
+exports.cloudinaryJson = cloudinaryJson;
