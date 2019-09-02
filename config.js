4273b3cf59af5761c8ab154a3912806676a5bc7e
Taran <taranpal.singh23@gmail.com>
2019-09-02 17:40:13 +0530

    config file added

---
+++ config.js
@@ -0,0 +1,29 @@
+const bufferJson ={
+  "access_token" : "2/4e63a0d53b91746132e844e5567c80feb383e85ae643c30d404c8016997c286b8c0e8a4bcc6778afff47bd7f567f71c2b3598e84dd44b1ab59e8a1106937a549",
+  "accounts" : {
+    "instagram" : "5d582feef5f92a3e3d30adf7",
+    "facebook" : "5d5c01b234f95b5f8d07ba36",
+    "twitter" : "5d58300df5f92a5a701384e3"
+}}
+
+const cloudinaryJson = { 
+  cloud_name: 'dclncnhmm', 
+  api_key: '792376593158319', 
+  api_secret: 'sKTr-LUijm5wGGzh1hHri9_7l-E' 
+}
+
+const myJsoon = {
+	"socialPost" : {
+		"font_family":"cursive",
+		"text": "Hello!!! #automation #bufferApi",
+		"backgroundImage":"https://source.unsplash.com/weekly?job(184 kB)",
+		"jobtitle":"Admin",
+		"companyName":"Cybergroup",
+		"location":"Noida",
+		"scheduled_at": "",
+"postsTo": ["twitter"]
+	}
+}
+
+exports.bufferJson = bufferJson;
+exports.cloudinaryJson = cloudinaryJson;
